cb8f3d8c86c96b132a2718aa53b7bd6ccdab8fb1
Ece Aslan <109793753+apolloxsun@users.noreply.github.com>
2023-12-02 12:16:16 +0300

    Create shapes.h
    header file for the structs used in main.c

---
+++ shapes.h
@@ -0,0 +1,16 @@
+#include "include/raylib.h"
+
+typedef struct{
+    int xCoordinate;
+    int yCoordinate;
+    float circleRadius;
+    Color circleColor;
+} Circle;
+
+typedef struct{
+    int xCoordinate;
+    int yCoordinate;
+    float xCoordinate2;
+    float yCoordinate2;
+    Color lineColor;
+} Line;
